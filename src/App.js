d3b405e019de57c431e14c2f00b7fc9dd56a8803
marcos8896 <elyomarcos@gmail.com>
2018-03-07 21:51:42 -0600

    Section 3 - Lecture 36 : Working with props.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person/>
+        <Person name="Marcos" age="21" />
+        <Person name="Diego" age="22"> My hobbies: Ser falso natural </Person>
+        <Person name="Brandon" age="21" />
       </div>
     );
   }
