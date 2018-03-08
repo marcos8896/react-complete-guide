64ec6a2c50987b210114119ccc3e200ad55287b0
marcos8896 <elyomarcos@gmail.com>
2018-03-07 22:22:33 -0600

    Section 3 - Lecture 39 : Props & State.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: "Marcos", age: 21 },
+      { name: "Diego", age: 22 },
+      { name: "Brandon", age: 21 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Marcos" age="21" />
-        <Person name="Diego" age="22"> My hobbies: Ser falso natural </Person>
-        <Person name="Brandon" age="21" />
+        <button>Switch names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Ser falso natural </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
