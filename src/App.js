670f16c07743b94c3d210fd025b9ad4b12f23332
marcos8896 <elyomarcos@gmail.com>
2018-04-20 20:52:45 -0500

    Section 5 - Lecture 66 : Enabling & Using CSS Modules.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -76,16 +76,16 @@ class App extends Component {
 
     }
 
-    let classes = [];
-    if (this.state.persons.length <= 2) classes.push('red');
+    let conditionalClasses = [];
+    if (this.state.persons.length <= 2) conditionalClasses.push( classes.red );
 
-    if (this.state.persons.length <= 1) classes.push('bold');
+    if (this.state.persons.length <= 1) conditionalClasses.push( classes.bold );
 
     return (
 
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>Mi estringsita :P</p>
+        <p className={conditionalClasses.join(' ')}>Mi estringsita :P</p>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle persons</button>
